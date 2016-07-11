10d6197b9a67a940f3a5de759b06bed752a1d7cd
Dariusz Gołąbek <golabiusz@gmail.com>
2016-07-11 07:42:04 +0200

    More redis config + some very simple tests with dump

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ A Symfony project created on July 6, 2016, 9:32 pm.
 
 Session handler requires `Redis >= 2.6.12` for LUA scripts and SET with options.
 
+## SncRedisBundle ##
+
+[SncRedisBundle docs](https://github.com/snc/SncRedisBundle/blob/master/Resources/doc/index.md)
+
+## Redis ##
+
+```redis-cli MONITOR```
+
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // Redis - session test
+        $this->get('session')->set('test', 'TEST');
+        $ses = $this->get('session')->get('test');
+        dump($ses);
+        // Redis - replication test
+        $this->get('snc_redis.cache')->incr('foo:bar');
+        $foo = $this->get('snc_redis.cache')->get('foo:bar');
+        dump($foo);
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
